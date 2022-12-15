6e10a6238f534133ac6bf509a01141c9d803027b
Rahmat Bakhshi <swishtransportation@gmail.com>
2022-12-15 16:29:27 -0500

    Final

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ app.get("/", (req, res) => {
   const posts = postBank.list();
 
   const html = `<div>
+
   <!DOCTYPE html>
-  <html>
-  <head>
+<html>
+<head>
     <title>Wizard News</title>
+
     <link rel="stylesheet" href="/style.css" />
   </head>
   <body>
@@ -22,6 +24,8 @@ app.get("/", (req, res) => {
           (post) => `
         <div class='news-item'>
           <p>
+          <a href="/posts/${post.id}">${post.title}</a>
+
             <span class="news-position">${post.id}. ▲</span>
             ${post.title}
             <small>(by ${post.name})</small>
@@ -44,8 +48,56 @@ app.use(express.static("public"));
 app.get("/posts/:id", (req, res) => {
   const id = req.params.id;
   const post = postBank.find(id);
-  res.send(`<h2>${post.title} ${post.name} ${post.date} ${post.content}}</h2>`);
-});
+  if (!post.id) {
+    res.status(404)
+    const html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <header><img src="/logo.png"/>Wizard News</header>
+      <div class="not-found">
+        <p>Accio Page! 🧙‍♀️ ... Page Not Found</p>
+        <img src="/dumbledore-404.gif" />
+      </div>
+    </body>
+    </html>`
+    res.send(html)
+  } else {
+
+  const html =
+  `<div>
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date} | ${post.content}
+          </small>
+        </div>
+
+    </div>
+  </body>
+</html>
+    </div>`;
+  res.send(html);
+}});
+
 
 const PORT = 1337;
 
